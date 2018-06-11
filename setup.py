5eceaccf0e42b710e8d417f4fb33c6b371ec3f06
pythonex <harrigenius19@gmail.com>
2018-06-11 09:15:25 +0800

    add metadata for package

--- setup.py
+++ setup.py
@@ -15,5 +15,15 @@ setuptools.setup(
       install_requires=[
             'requests',
       ],
+      classifiers=[
+          'Development Status :: 1 - Development/Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Software Development :: Libraries',
+      ],
       url='https://github.com/pythonguru101/fraction-chart/',
 )
